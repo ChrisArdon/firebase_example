3aa67528d06d708fc899636ee6857e5608cb80be
Chris Ardon <rockrisbl@gmail.com>
2021-11-04 18:56:39 -0600

    config/firebase.js agregado

---
+++ config/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+export default{
+    firebaseConfig = {
+        apiKey: "AIzaSyAgMTVYDGE7_1EDA8p6a62_GvcEtIBlDeE",
+        authDomain: "fir-example-d352e.firebaseapp.com",
+        projectId: "fir-example-d352e",
+        storageBucket: "fir-example-d352e.appspot.com",
+        messagingSenderId: "204515384311",
+        appId: "1:204515384311:web:001690fa654ac642736cf2"
+      }
+}
